db9527b6103ea4bcea1459d688b650dac2246ae7
kotemaru <kotemaru@kotemaru.org>
2015-09-21 17:00:54 +0900

    TaskBoard2

--- mymine/war/mymine/js/TaskBoard2.js
+++ mymine/war/mymine/js/TaskBoard2.js
@@ -6,11 +6,11 @@ function TaskBoard() {
 	Class.load = function() {
 		var opts = {};
 		var query = null;
-		opts.project_id     = Control.getProjectId();
+		opts.project_id = Control.getProjectId();
 		opts.assigned_to_id = Control.getFilterUser();
-		opts.status_id      = Control.getFilterClosed();
+		opts.status_id = Control.getFilterClosed();
 		Control.getFilterMasters(opts);
-		opts.limit=100;
+		opts.limit = 100;
 
 		RedMine.getIssues(function(resData) {
 			var issues = resData.issues;
@@ -24,6 +24,7 @@ function TaskBoard() {
 					var storyId = issue.parent.id;
 					var story = createStory(storys, storyId);
 					story.status[issue.status.id][issue.id] = issue;
+					story.children.push(issue);
 					story.childCount++;
 				}
 				MasterTable.register(issue);
@@ -33,12 +34,10 @@ function TaskBoard() {
 			for ( var storyId in storys) {
 				storyArray.push(storys[storyId]);
 			}
-			storyArray.sort(function(a,b){
+			storyArray.sort(function(a, b) {
 				if (b.issue == null || a.issue == null) return 0;
 				return b.issue.priority.id - a.issue.priority.id;
 			});
-
-
 
 			$("#taskBoardTable").html($("<tbody/>"));
 			var $table = $("#taskBoardTable>tbody");
@@ -46,32 +45,28 @@ function TaskBoard() {
 			var $tr = append($table, "<tr/>");
 			var $td = append($tr, "<th/>");
 			$td.text("Story");
-			for ( var id in statuses.values) {
-				var $td = append($tr, "<th/>");
-				// console.log("statuses:",statuses.values[id]);
-				$td.text(statuses.values[id]);
+
+			var $td = append($tr, "<th style='text-align:left;'/>");
+			var statuses = MasterTable.getStatuses();
+			for ( var i=0 ; i<STATE_MAP.length; i++) {
+				$td.append($("<span style='display:inline-block;width:200px;text-align:center;'>"+STATE_MAP[i].name+"</span>"));
 			}
 
-			for ( var i=0; i<storyArray.length; i++) {
-				// console.log("storyId:",storyId);
+			for (var i = 0; i < storyArray.length; i++) {
 				var story = storyArray[i];
 				var $tr = append($table, "<tr/>");
 				var $td = append($tr, "<td/>");
 				if (story.issue != null && story.childCount > 0) {
 					$td.append(createKanban(story.issue));
 				}
-				for ( var stateId in story.status) {
-					var $td = append($tr, "<td/>");
-					for ( var taskId in story.status[stateId]) {
-						// console.log("taskId:",taskId);
-						var task = story.status[stateId][taskId];
-						$td.append(createKanban(task));
+				var $td = append($tr, "<td/>");
+				//console.log("story.children.length:",story.children.length,story.childCount);
+				if (story.children.length > 0) {
+					for (var j = 0; j < story.children.length; j++) {
+						$td.append(createKanban(story.children[j],{tab:true}));
 					}
-					// console.log("taskId:",story.childCount,
-					// story.issue.status.id, stateId);
-					if (story.issue != null && story.childCount == 0 && story.issue.status.id == stateId) {
-						$td.append(createKanban(story.issue));
-					}
+				} else if (story.issue != null) {
+					$td.append(createKanban(story.issue,{tab:true}));
 				}
 			}
 		}, query, opts);
@@ -86,7 +81,8 @@ function TaskBoard() {
 	function createStory(storys, storyId) {
 		if (storys[storyId] != null) return storys[storyId];
 		var story = {
-			status : {}
+			status : {},
+			children : []
 		};
 		var statuses = MasterTable.getStatuses();
 		for ( var id in statuses.values) {
@@ -97,13 +93,13 @@ function TaskBoard() {
 		return story;
 	}
 
-	function createKanban(issue) {
+	function createKanban(issue, opt) {
 		var $TEMPLATE = $("#templ_kanban");
 		var $kanban = $TEMPLATE.clone();
 		$kanban.find("#ticketId").text(issue.id);
 		$kanban.find("#title").text(issue.subject);
 		$kanban.find("#assigned").text(issue.assigned_to ? issue.assigned_to.name : "");
-		$kanban.find("#assignedIcon").toggle(issue.assigned_to!=null);
+		$kanban.find("#assignedIcon").toggle(issue.assigned_to != null);
 
 		var colors = {
 			1 : "#e0e0ff",
@@ -123,6 +119,10 @@ function TaskBoard() {
 		if (tracker == "Task" || tracker == "タスク") icon = "img/led24/doc_page.png";
 		$kanban.find("#tracker").attr("src", icon);
 
+		if (opt && opt.tab) {
+			var index = getStateIndex(issue);
+			$kanban.css("margin-left", (index*200)+"px");
+		}
 		return $kanban;
 	}
 
@@ -133,5 +133,21 @@ function TaskBoard() {
 		selection.collapse(document.body, 0);
 		RedMine.openIssue(ticketId + "/edit");
 	});
+
+	function getStateIndex(issue) {
+		for (var i=0;i<STATE_MAP.length; i++) {
+			if (STATE_MAP[i].name == issue.status.name) return i;
+		}
+		return STATE_MAP.length-1;
+	}
+
+	var STATE_MAP =
+	[
+		 {name:"新規"},
+		 {name:"進行中"},
+		 {name:"解決"},
+		 {name:"終了"},
+		 {name:"その他"}
+	];
 
 })(TaskBoard);
